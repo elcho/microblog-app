12b9c2a0b6c566ebc6a9b3fd575f146842743063
elcho <elchononspiegel@gmail.com>
2018-03-28 12:47:10 -0400

    user registration

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import LoginForm
+from app import db
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user
 from flask_login import logout_user
 from flask_login import login_required
@@ -15,7 +16,7 @@ from werkzeug.urls import url_parse
 def index():
     posts = [
         {
-            'author': {'username': 'John'},
+            'author': current_user.username,
             'body': 'Beautiful day in Lakewood!'
         },
         {
@@ -36,7 +37,7 @@ def wisdom():
             'message': 'A calm and honest life brings more happiness than the pursuit of success and restlessness.'
         }
     ]
-    return render_template('wisdom.html', title='Home',  messages=messages)
+    return render_template('wisdom.html', title='Home', messages=messages)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -49,7 +50,7 @@ def login():
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
-        login_user(user, remember=form.remeber_me.data)
+        login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
@@ -62,3 +63,18 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
