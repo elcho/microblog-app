100cb4e9d6427fc0e8e1f12161ddd6ccf8db7663
elcho <elchononspiegel@gmail.com>
2018-03-29 11:08:28 -0400

    added followers functionality and models, added username validation (in right place)

--- app/forms.py
+++ app/forms.py
@@ -19,6 +19,21 @@ class RegistrationForm(FlaskForm):
         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+
 
 class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -26,14 +41,5 @@ class EditProfileForm(FlaskForm):
     submit = SubmitField('Submit')
 
 
-def validate_username(self, username):
-    user = User.query.filter_by(username=username.data).first()
-    if user is not None:
-        raise ValidationError('Please use a different username.')
 
-
-def validate_email(self, email):
-    user = User.query.filter_by(email=email.data).first()
-    if user is not None:
-        raise ValidationError('Please use a different email address.')
 
