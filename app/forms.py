3843ecf017ce4d71826ee1c11cd01fa8b16a3649
elcho <elchononspiegel@gmail.com>
2018-03-30 10:37:15 -0400

    more stuff

--- app/forms.py
+++ app/forms.py
@@ -19,12 +19,8 @@ class RegistrationForm(FlaskForm):
         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
-    def __init__(self, original_username, *args, **kwargs):
-        super(EditProfileForm, self).__init__(*args, **kwargs)
-        self.original_username = original_username
-
     def validate_username(self, username):
-        if username.data != self.original_username:
+        # if username.data != self.original_username:
             user = User.query.filter_by(username=username.data).first()
             if user is not None:
                 raise ValidationError('Please use a different username.')
@@ -51,5 +47,7 @@ class EditProfileForm(FlaskForm):
                 raise ValidationError('Please use a different username.')
 
 
-
-
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something', validators=[
+        DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Submit')
