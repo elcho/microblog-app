19d9e5e653b5735daf70926145dba113ed0dad8d
elcho <elchononspiegel@gmail.com>
2018-04-09 14:18:45 -0400

    debug file, touch up email

--- app/email.py
+++ app/email.py
@@ -16,7 +16,8 @@ def send_password_reset_email(user):
     send_email('[Microblog] Reset Your Password',
                sender=app.config['ADMINS'][0],
                recipients=[user.email],
-               text_body=render_template('email/reset_password.txt',
+               text_body=render_template('email/reset_password',
                                          user=user, token=token),
                html_body=render_template('email/reset_password.html',
-                                         user=user, token=token))
+                                         user=user, token=token)
+               )
