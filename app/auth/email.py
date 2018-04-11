1dcbf8b177912596d4bd2de57100f10a383f68d8
elcho <elchononspiegel@gmail.com>
2018-04-11 12:17:31 -0400

    .env file, some more refactoring fixes

--- app/auth/email.py
+++ app/auth/email.py
@@ -1,25 +1,11 @@
-from flask_mail import Message
-from flask import render_template
-from app import mail
-from threading import Thread
-
-
-def send_email(subject, sender, recipients, text_body, html_body):
-    msg = Message(subject, sender=sender, recipients=recipients)
-    msg.body = text_body
-    msg.html = html_body
-    Thread(target=send_async_email, args=(app, msg)).start()
-
-
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)
+from flask import render_template, current_app
+from app.email import send_email
 
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
     send_email('[Microblog] Reset Your Password',
-               sender=app.config['ADMINS'][0],
+               sender=current_app.config['ADMINS'][0],
                recipients=[user.email],
                text_body=render_template('email/reset_password',
                                          user=user, token=token),
